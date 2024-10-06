2f9160e47c06a4823414049d8326127fff05f0f2
Wenielrana07 <rana.weniel@gmail.com>
2024-10-06 10:40:41 +0800

    added script on applet 2

--- applet0/script2.js
+++ applet0/script2.js
@@ -29,12 +29,10 @@ class LeafletMap {
     }
 }
 
-const myMap = new LeafletMap('map', [8.360004, 124.868419], 18);
+const myMap = new LeafletMap('map', [8.4003566, 124.8780386], 16);
 
 /*
-myMap.addMarker(8.359735, 124.869206, 'CCS Faculty Office');
-myMap.addMarker(8.359639,124.869179, 'CCS Laboratory 1');
-myMap.addMarker(8.359554,124.869153, 'CCS Laboratory 2');
+myMap.addMarker (8.4003566, 124.8780386, 'My home');
 */
 
 myMap.loadMarkersFromJson('applet-2.json');
